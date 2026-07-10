01edb64fc6fb9895148231856e6b1cf1dd652e06
CompuGeniusCode <contact@cgprograms.com>
2026-07-10 01:22:05 -0400

    Combat: decompile 54 more non-leaf functions (+ caller fix)

--- src/Combat/Main/InitSubObject0203bd78.cpp
+++ src/Combat/Main/InitSubObject0203bd78.cpp
@@ -1,8 +1,8 @@
 #include <globaldefs.h>
 
-extern "C" void func_0203c304(void*);
+void ResetBattleState0203c304(char*);
 
 // USA: func_0203bd78
 ARM void InitSubObject0203bd78(char* obj) {
-    func_0203c304(obj + 0x10);
+    ResetBattleState0203c304(obj + 0x10);
 }
