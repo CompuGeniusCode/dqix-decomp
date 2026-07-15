e6921a12c37bffedcebe009c01fc25651ed5dd83
CompuGeniusCode <contact@cgprograms.com>
2026-07-15 15:57:14 -0400

    Match batch of 1 ov001 overlay functions

---
+++ src/Combat/Overlay_1/SetFlagFromField424Nonzero_02163c28.cpp
@@ -0,0 +1,16 @@
+#include <globaldefs.h>
+#include "Combat/Main/BattleList.h"
+
+void* GetFieldPtrAt0x5ca4(void* obj);
+extern "C" int func_ov017_021d6134(void* obj, int flag);
+
+// USA: func_ov001_02163c28
+ARM int SetFlagFromField424Nonzero_02163c28(void* obj) {
+    void* p = *(void**)GetFieldPtrAt0x5ca4(GetBattleStruct());
+    int flag = 0;
+    if (p != NULL) {
+        flag = *(void**)((char*)p + 0x424) != NULL;
+    }
+    func_ov017_021d6134(obj, flag);
+    return 1;
+}
