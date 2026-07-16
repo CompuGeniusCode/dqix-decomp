2674d0139f3db856c44163c8fe6d67c0e36568ef
CompuGeniusCode <contact@cgprograms.com>
2026-07-16 12:59:53 -0400

    Match batch of 3 ov025 overlay functions

---
+++ src/Combat/Overlay_25/GetByte_021dcc64_021dcc64.cpp
@@ -0,0 +1,7 @@
+#include <globaldefs.h>
+
+// USA: func_ov025_021dcc64
+ARM unsigned char GetByte_021dcc64_021dcc64(void* obj) {
+    unsigned char* base = (unsigned char*)obj;
+    return (base + 0x6000)[0xe4b];
+}
