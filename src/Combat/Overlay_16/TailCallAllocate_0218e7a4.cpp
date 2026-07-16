b9a57bcc7e9340002e4b898c5e6b8a4c1c96d439
CompuGeniusCode <contact@cgprograms.com>
2026-07-16 13:00:30 -0400

    Match batch of 3 ov016 overlay functions

---
+++ src/Combat/Overlay_16/TailCallAllocate_0218e7a4.cpp
@@ -0,0 +1,8 @@
+#include <globaldefs.h>
+
+void* SafeAllocatorAllocate_0218bc8c(unsigned int);
+
+// USA: func_ov016_0218e7a4
+ARM void* TailCallAllocate_0218e7a4(unsigned int len) {
+    return SafeAllocatorAllocate_0218bc8c(len);
+}
