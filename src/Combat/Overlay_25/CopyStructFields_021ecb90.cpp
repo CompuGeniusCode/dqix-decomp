54df8a344160c503c742ea75f2ebe0224298624a
CompuGeniusCode <contact@cgprograms.com>
2026-07-16 02:36:18 -0400

    Match batch of 18 ov025 overlay functions

---
+++ src/Combat/Overlay_25/CopyStructFields_021ecb90.cpp
@@ -0,0 +1,40 @@
+#include <globaldefs.h>
+
+struct Struct021ecb90 {
+    int a;
+    unsigned char b, c, d;
+    int e, f, g, h, i, j;
+    short k, l;
+    int m, n;
+    unsigned short o, p, q, r, s, t;
+    unsigned char u, v, w;
+    unsigned short x;
+};
+
+// USA: func_ov025_021ecb90
+ARM void CopyStructFields_021ecb90(struct Struct021ecb90* dst, struct Struct021ecb90* src) {
+    dst->a = src->a;
+    dst->b = src->b;
+    dst->c = src->c;
+    dst->d = src->d;
+    dst->e = src->e;
+    dst->f = src->f;
+    dst->g = src->g;
+    dst->h = src->h;
+    dst->i = src->i;
+    dst->j = src->j;
+    dst->k = src->k;
+    dst->l = src->l;
+    dst->m = src->m;
+    dst->n = src->n;
+    dst->o = src->o;
+    dst->p = src->p;
+    dst->q = src->q;
+    dst->r = src->r;
+    dst->s = src->s;
+    dst->t = src->t;
+    dst->u = src->u;
+    dst->v = src->v;
+    dst->w = src->w;
+    dst->x = src->x;
+}
