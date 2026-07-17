40d245e9917c95aec56a07bdc05723bf0ed47184
CompuGeniusCode <contact@cgprograms.com>
2026-07-16 22:09:14 -0400

    Match batch of 20 ov031 overlay functions

---
+++ src/Combat/Overlay_31/GetField44_0221f994.cpp
@@ -0,0 +1,9 @@
+#include <globaldefs.h>
+
+struct GlobalStruct_0221f994 { char pad[0x2c]; int field44; };
+extern GlobalStruct_0221f994 data_ov031_0224f670;
+
+// USA: func_ov031_0221f994
+ARM int GetField44_0221f994(void) {
+    return data_ov031_0224f670.field44;
+}
