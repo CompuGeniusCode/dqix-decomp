17b064b26a6915850de7a86ff253b6f66c312275
CompuGeniusCode <contact@cgprograms.com>
2026-07-17 00:15:15 -0400

    Match batch of 18 ov031 overlay functions

---
+++ src/Combat/Overlay_31/CopyFrom027ffcf4.cpp
@@ -0,0 +1,10 @@
+#include <globaldefs.h>
+
+ARM void CopyFrom027ffcf4(void* dst);
+
+// USA: func_ov031_022205d8
+THUMB
+int LoadDataAndReturnTrue_022205d8(void* dst) {
+	CopyFrom027ffcf4(dst);
+	return 1;
+}
