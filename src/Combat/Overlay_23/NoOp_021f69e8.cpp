191992330772cb58fd755df494dab33408eb6062
CompuGeniusCode <contact@cgprograms.com>
2026-07-16 21:04:48 -0400

    Match batch of 20 ov023 overlay functions

---
+++ src/Combat/Overlay_23/NoOp_021f69e8.cpp
@@ -0,0 +1,5 @@
+#include <globaldefs.h>
+
+// USA: func_ov023_021f69e8
+ARM void NoOp_021f69e8(void) {
+}
---
+++ src/Combat/Overlay_23/ReturnZero_021f6ab8.cpp
@@ -0,0 +1,6 @@
+#include <globaldefs.h>
+
+// USA: func_ov023_021f6ab8
+ARM int ReturnZero_021f6ab8(void) {
+    return 0;
+}
