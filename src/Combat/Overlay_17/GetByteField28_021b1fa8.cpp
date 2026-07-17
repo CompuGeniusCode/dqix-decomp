926345a65d5194a654d7fa7dd71ab96c03d8ff4c
CompuGeniusCode <contact@cgprograms.com>
2026-07-16 21:03:11 -0400

    Match batch of 10 ov017 overlay functions

---
+++ src/Combat/Overlay_17/GetByteField28_021b1fa8.cpp
@@ -0,0 +1,6 @@
+#include <globaldefs.h>
+
+// USA: func_ov017_021b1fa8
+ARM unsigned char GetByteField28_021b1fa8(void* obj) {
+	return ((unsigned char*)obj)[0x28];
+}
---
+++ src/Combat/Overlay_17/NoOp_021b1fa0.cpp
@@ -0,0 +1,5 @@
+#include <globaldefs.h>
+
+// USA: func_ov017_021b1fa0
+ARM void NoOp_021b1fa0(void) {
+}
