cf33d7ee162cc91e0297ca1b4b3b7b641080326d
CompuGeniusCode <contact@cgprograms.com>
2026-07-16 22:05:01 -0400

    Match batch of 20 ov024 overlay functions

---
+++ src/Combat/Overlay_24/NoOp_021fc690.cpp
@@ -0,0 +1,5 @@
+#include <globaldefs.h>
+
+// USA: func_ov024_021fc690
+ARM void NoOp_021fc690(void) {
+}
---
+++ src/Combat/Overlay_24/NoOp_021fc694.cpp
@@ -0,0 +1,5 @@
+#include <globaldefs.h>
+
+// USA: func_ov024_021fc694
+ARM void NoOp_021fc694(void) {
+}
