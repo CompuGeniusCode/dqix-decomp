bec83e736ca95dae5d974832d4ef33de7e74a4ff
CompuGeniusCode <contact@cgprograms.com>
2026-07-28 18:47:47 -0400

    Match batch of 5 arm9 main functions

---
+++ src/Combat/Main/GetGlobalPtr_0202ae18.cpp
@@ -0,0 +1,8 @@
+#include <globaldefs.h>
+
+extern int data_020fefec;
+
+// USA: func_0202ae18  (semantic: GetGlobalPtr_0202ae18)
+extern "C" ARM void* func_0202ae18(void) {
+    return &data_020fefec;
+}
---
+++ src/Combat/Main/GetGlobalPtr_0203bd08.cpp
@@ -0,0 +1,8 @@
+#include <globaldefs.h>
+
+extern int data_02105ab4;
+
+// USA: func_0203bd08  (semantic: GetGlobalPtr_0203bd08)
+extern "C" ARM void* func_0203bd08(void) {
+    return &data_02105ab4;
+}
