89f207f7643119160727ecebb543f1929c878c52
CompuGeniusCode <contact@cgprograms.com>
2026-07-28 12:39:50 -0400

    Match batch of 2 ov016 overlay functions

---
+++ src/Combat/Overlay_16/InitAndReturnSelf_0218e790.cpp
@@ -0,0 +1,10 @@
+#include <globaldefs.h>
+
+struct Obj_0218e790;
+extern "C" void func_0200edb4(Obj_0218e790* obj);
+
+// USA: func_ov016_0218e790  (semantic: InitAndReturnSelf_0218e790)
+extern "C" ARM Obj_0218e790* func_ov016_0218e790(Obj_0218e790* obj) {
+    func_0200edb4(obj);
+    return obj;
+}
