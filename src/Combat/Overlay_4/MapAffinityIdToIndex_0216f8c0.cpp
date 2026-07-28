7ceee05cd6c41e4d7cdbccff4a9d8c3789d4e638
CompuGeniusCode <contact@cgprograms.com>
2026-07-28 02:22:46 -0400

    Match batch of 1 ov004 overlay functions

---
+++ src/Combat/Overlay_4/MapAffinityIdToIndex_0216f8c0.cpp
@@ -0,0 +1,21 @@
+#include <globaldefs.h>
+
+// USA: func_ov004_0216f8c0  (semantic: MapAffinityIdToIndex_0216f8c0)
+extern "C" ARM unsigned short func_ov004_0216f8c0(int v) {
+    int r = 0;
+    if (v == 0x3e8) r = 0x45;
+    if (v == 0x3e9) r = 0x46;
+    if (v == 0x3ea) r = 0x47;
+    if (v == 0x3eb) r = 0x48;
+    if (v == 0x3ec) r = 0x49;
+    if (v == 0x3ed) r = 0x4a;
+    if (v == 0x3ee) r = 0x4b;
+    if (v == 0x3ef) r = 0x4c;
+    if (v == 0x3f0) r = 0x4d;
+    if (v == 0x3f1) r = 0x4e;
+    if (v == 0x3f2) r = 0x4f;
+    if (v == 0x3f3) r = 0x50;
+    if (v == 0x3f4) r = 0x51;
+    if (v == 0x3f5) r = 0x52;
+    return (unsigned short)r;
+}
