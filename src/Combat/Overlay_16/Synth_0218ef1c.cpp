1af6cd5031331a4303e7e02092647c3047b9b970
CompuGeniusCode <contact@cgprograms.com>
2026-08-04 22:48:09 -0400

    Match batch of 5 ov016 overlay functions

---
+++ src/Combat/Overlay_16/Synth_0218ef1c.cpp
@@ -0,0 +1,6 @@
+#include <globaldefs.h>
+
+// USA: func_ov016_0218ef1c  (semantic: Synth_0218ef1c)  (semantic: Synth_0218ef1c)
+extern "C" ARM int func_ov016_0218ef1c(void* p) {
+    return *(int*)((char*)p + 0x18);
+}
---
+++ src/Combat/Overlay_16/Synth_0218ef24.cpp
@@ -0,0 +1,6 @@
+#include <globaldefs.h>
+
+// USA: func_ov016_0218ef24  (semantic: Synth_0218ef24)  (semantic: Synth_0218ef24)
+extern "C" ARM int func_ov016_0218ef24(void* p) {
+    return *(int*)((char*)p + 0xc);
+}
