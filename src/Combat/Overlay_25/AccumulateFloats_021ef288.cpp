d63af17b872a6457383ddfefd7f9f62d47e813cb
CompuGeniusCode <contact@cgprograms.com>
2026-08-18 16:09:19 -0400

    Match batch of 3 ov025 overlay functions

---
+++ src/Combat/Overlay_25/AccumulateFloats_021ef288.cpp
@@ -0,0 +1,22 @@
+#include <globaldefs.h>
+
+#pragma define_section initcode ".init" RX
+
+extern float data_ov025_021ef464[8];
+extern float data_ov025_021ef988[8];
+
+// USA: func_ov025_021ef288  (semantic: AccumulateFloats_021ef288)
+extern "C" __declspec(initcode) ARM void func_ov025_021ef288(void) {
+    float t;
+    float a;
+    float b;
+    data_ov025_021ef988[0] = data_ov025_021ef464[1] + (data_ov025_021ef464[3] + (data_ov025_021ef464[2] + data_ov025_021ef464[4]));
+    t = data_ov025_021ef988[5] + data_ov025_021ef464[2];
+    a = data_ov025_021ef464[4];
+    data_ov025_021ef988[6] = t;
+    t = t + a;
+    b = data_ov025_021ef464[3];
+    data_ov025_021ef988[1] = t;
+    t = t + b;
+    data_ov025_021ef988[4] = t;
+}
---
+++ src/Combat/Overlay_25/AccumulateFloats_021ef310.cpp
@@ -0,0 +1,22 @@
+#include <globaldefs.h>
+
+#pragma define_section initcode ".init" RX
+
+extern float data_ov025_021ef898[8];
+extern float data_ov025_021ef9c4[8];
+
+// USA: func_ov025_021ef310  (semantic: AccumulateFloats_021ef310)
+extern "C" __declspec(initcode) ARM void func_ov025_021ef310(void) {
+    float t;
+    float a;
+    float b;
+    data_ov025_021ef9c4[3] = data_ov025_021ef898[4] + (data_ov025_021ef898[3] + (data_ov025_021ef898[6] + data_ov025_021ef898[5]));
+    t = data_ov025_021ef9c4[1] + data_ov025_021ef898[6];
+    a = data_ov025_021ef898[5];
+    data_ov025_021ef9c4[2] = t;
+    t = t + a;
+    b = data_ov025_021ef898[3];
+    data_ov025_021ef9c4[4] = t;
+    t = t + b;
+    data_ov025_021ef9c4[0] = t;
+}
