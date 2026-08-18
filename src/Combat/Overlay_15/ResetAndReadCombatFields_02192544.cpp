a00267fb7cf92ab5af72ea7fc4e6c1f14cc1748e
CompuGeniusCode <contact@cgprograms.com>
2026-08-18 01:03:22 -0400

    Match batch of 5 ov015 overlay functions

---
+++ src/Combat/Overlay_15/ResetAndReadCombatFields_02192544.cpp
@@ -0,0 +1,27 @@
+#include <globaldefs.h>
+
+struct BattleStruct* GetBattleStruct(void);
+int GetFieldIfFlag2(char* obj);
+struct Struct020A2CF0;
+void ResetState020a2cf0(struct Struct020A2CF0* obj);
+struct S020a3568;
+void SetField0x218(struct S020a3568* p, int v);
+void ReadFields0x70To0x78(unsigned char* obj, int* a, int* b, int* c);
+extern "C" void func_0202e5d8(void* obj, int a, int b, int c);
+
+// USA: func_ov015_02192544  (semantic: ResetAndReadCombatFields_02192544)
+extern "C" ARM void func_ov015_02192544(void) {
+    struct BattleStruct* battle = GetBattleStruct();
+    if (battle == 0) {
+        return;
+    }
+    int obj = GetFieldIfFlag2((char*)battle);
+    if (obj == 0) {
+        return;
+    }
+    ResetState020a2cf0((struct Struct020A2CF0*)obj);
+    SetField0x218((struct S020a3568*)obj, 2);
+    int a, b, c;
+    ReadFields0x70To0x78((unsigned char*)obj, &a, &b, &c);
+    func_0202e5d8((void*)obj, a, 0x13d7, c);
+}
