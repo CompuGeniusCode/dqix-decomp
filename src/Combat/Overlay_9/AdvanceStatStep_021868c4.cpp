92e4d09f091166c8b2f5e22e051e9e1d3ccb30dd
CompuGeniusCode <contact@cgprograms.com>
2026-08-19 07:19:35 -0400

    Match batch of 5 ov009 overlay functions

---
+++ src/Combat/Overlay_9/AdvanceStatStep_021868c4.cpp
@@ -0,0 +1,58 @@
+#include <globaldefs.h>
+
+extern "C" void func_ov009_02188c2c(void *self);
+extern "C" void func_ov009_02188d9c(void *self);
+extern "C" void func_ov009_02188e70(void *self, unsigned char val);
+extern "C" void func_ov009_02188944(void *self, unsigned char val);
+extern "C" int func_ov009_02188ee8(void *self);
+extern "C" ARM void _Z20SetStatValue021855dcPhi(unsigned char *self, int val);
+
+// USA: func_ov009_021868c4  (semantic: AdvanceStatStep_021868c4)
+extern "C" ARM void func_ov009_021868c4(unsigned char *self) {
+	unsigned char step = self[0xc59];
+	if (step == 0) {
+		func_ov009_02188c2c(self);
+		func_ov009_02188d9c(self);
+		int idx = self[0xda3];
+		unsigned char val = *(self + idx + 0xdac);
+		func_ov009_02188e70(self, val);
+		*(unsigned int*)(self + 0xd9c) |= 1;
+		self[0xc59] = self[0xc59] + 1;
+		return;
+	}
+	if (step == 1) {
+		if (*(unsigned int*)(self + 0xd9c) & 0x80) {
+			return;
+		}
+		int idx = self[0xda3];
+		unsigned char val = *(self + idx + 0xdac);
+		func_ov009_02188944(self, val);
+		self[0xc59] = self[0xc59] + 1;
+		return;
+	}
+	if (step != 2) {
+		return;
+	}
+	int result = func_ov009_02188ee8(self);
+	if (result == -2) {
+		_Z20SetStatValue021855dcPhi(self, 4);
+		return;
+	}
+	if (result == -3) {
+		_Z20SetStatValue021855dcPhi(self, 2);
+		return;
+	}
+	if (result < 0x65 || result > 0x6c) {
+		return;
+	}
+	int idx2 = self[0xda3];
+	int v = result - 0x64;
+	signed char *arr = (signed char*)(self + 0xda0);
+	if (v > arr[idx2]) {
+		return;
+	}
+	if (v == *(signed char*)(self + 0xc58)) {
+		return;
+	}
+	_Z20SetStatValue021855dcPhi(self, v & 0xff);
+}
---
+++ src/Combat/Overlay_9/SyncCombatantAndFlushMatrix_02184c30.cpp
@@ -0,0 +1,23 @@
+#include <globaldefs.h>
+
+extern "C" void func_ov023_021e5628(void* obj, void* ctx);
+extern "C" void func_02035a2c(void* obj, int flag);
+
+// USA: func_ov009_02184c30  (semantic: SyncCombatantAndFlushMatrix_02184c30)
+extern "C" ARM void func_ov009_02184c30(void* objRaw) {
+    char* obj = (char*)objRaw;
+    signed char id = *(signed char*)(obj + 0xc58);
+    if (id == 0 || id == 0xc) {
+        return;
+    }
+
+    *(int*)0x4000444 = 0;
+    func_ov023_021e5628(*(void**)(obj + 0x7f8), 0);
+
+    if (*(int*)(obj + 0xd9c) & 0x200) {
+        func_02035a2c(obj + 0x878, 1);
+        func_02035a2c(obj + 0xcd8, 1);
+    }
+
+    *(int*)0x4000448 = 1;
+}
