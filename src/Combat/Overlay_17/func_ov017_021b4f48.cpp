3dfffe26af00ca510a9c5a3bfdb2357953860e16
CompuGeniusCode <contact@cgprograms.com>
2026-08-19 22:57:34 -0400

    Match batch of 4 ov017 overlay functions

---
+++ src/Combat/Overlay_17/func_ov017_021b4f48.cpp
@@ -0,0 +1,62 @@
+#include <globaldefs.h>
+#include "Combat/Main/BattleList.h"
+#include "Filesystem/BackgroundLoader.h"
+#include "Grotto/Main/ActiveGrottoClass.h"
+
+struct StreamHeader;
+
+extern "C" char* func_02012fe4(void);
+extern "C" int _Z17GetField5cb0ValuePc(char* battleStruct);
+extern "C" int _Z17GetField5cb4ValuePc(char* battleStruct);
+extern "C" int _Z17IsInRange0201b5b0i(int monsterId);
+extern "C" void _Z33SetupAndRunBufferedScript0209bc98PvP12StreamHeaderiiiii(
+    void* dest, struct StreamHeader* data, int size, int param,
+    int monsterId, int field5cb0, int field5cb4);
+
+extern const char data_ov017_021d7b71;
+
+struct ScriptSource_021b4f48 {
+    unsigned short monsterId;
+    char pad2[0x10 - 0x2];
+    int param;
+};
+
+struct ScriptRequest_021b4f48 {
+    char pad0[8];
+    struct ScriptSource_021b4f48* source;
+    int taskId;
+    unsigned char state;
+};
+
+// USA: func_ov017_021b4f48
+extern "C" ARM void func_ov017_021b4f48(struct ScriptRequest_021b4f48* request) {
+    struct BattleStruct* battle = GetBattleStruct();
+    BackgroundLoader* loader = BackgroundLoader::GetInstance();
+    ActiveGrottoClass* grotto = (ActiveGrottoClass*)((char*)func_02012fe4() + 0x23ec);
+
+    if (loader->GetTaskStatus(request->taskId) == 0) {
+        return;
+    }
+    if (loader->GetDetailedTaskStatus(request->taskId) == BackgroundLoader::TaskStatus_Complete) {
+        unsigned int size;
+        void* data;
+        loader->GetLoadedFileByID(request->taskId, &data, &size);
+        if (data != 0) {
+            int monsterId = request->source->monsterId;
+            int field5cb0 = _Z17GetField5cb0ValuePc((char*)battle);
+            int field5cb4 = _Z17GetField5cb4ValuePc((char*)battle);
+            if (_Z17IsInRange0201b5b0i(monsterId)) {
+                int rank = grotto->GetFloorMonsterRank(monsterId % 20);
+                int environ = grotto->GetActiveGrottoEnviron();
+                monsterId = rank + (environ * 100 + 0x9c40);
+            }
+            _Z33SetupAndRunBufferedScript0209bc98PvP12StreamHeaderiiiii(
+                (char*)request->source + 0x60, (struct StreamHeader*)data, size,
+                request->source->param, monsterId, field5cb0, field5cb4);
+        }
+    }
+    loader->RemoveTask(request->taskId);
+    request->taskId = -1;
+    request->taskId = loader->QueueLoadFile(&data_ov017_021d7b71, 0);
+    request->state = BackgroundLoader::TaskStatus_Fence;
+}
---
+++ src/Combat/Overlay_17/func_ov017_021c3fb4.cpp
@@ -0,0 +1,58 @@
+#include <globaldefs.h>
+#include "Combat/Main/BattleList.h"
+
+struct Field150Holder02052e14;
+short* GetField150Ptr0x488(struct Field150Holder02052e14* obj);
+void* GetData02100044(void);
+extern "C" void func_0205e330(void* a, void* b, int c);
+
+struct Evt021c3fb4 {
+    unsigned char tag;
+    unsigned char pad[3];
+    unsigned char combatantId : 4;
+    unsigned char flag : 1;
+    unsigned char slots[5];
+    short values[5];
+};
+
+// USA: func_ov017_021c3fb4
+extern "C" ARM void func_ov017_021c3fb4(int combatantId, int flag) {
+    struct BattleStruct* bs = GetBattleStruct();
+    struct CombatantStruct* c = GetCombatantWithFlag0x100(bs, combatantId);
+    if (c == NULL) return;
+
+    short* list = GetField150Ptr0x488((struct Field150Holder02052e14*)c);
+    if (list == NULL) return;
+
+    void* data = GetData02100044();
+
+    struct Evt021c3fb4 evt;
+    evt.tag = 2;
+    evt.combatantId = combatantId;
+
+    evt.slots[0] = 0;
+    evt.values[0] = list[0];
+    evt.slots[1] = 1;
+    evt.values[1] = list[1];
+    evt.slots[2] = 2;
+    evt.values[2] = list[2];
+    evt.slots[3] = 3;
+    evt.values[3] = list[3];
+    evt.slots[4] = 4;
+    evt.values[4] = list[4];
+    evt.flag = 0;
+    func_0205e330(data, &evt, 0);
+
+    evt.slots[0] = 5;
+    evt.values[0] = list[5];
+    evt.slots[1] = 6;
+    evt.values[1] = list[6];
+    evt.slots[2] = 7;
+    evt.values[2] = list[7];
+    evt.slots[3] = 8;
+    evt.values[3] = list[8];
+    evt.slots[4] = 9;
+    evt.values[4] = list[9];
+    evt.flag = flag;
+    func_0205e330(data, &evt, 0);
+}
