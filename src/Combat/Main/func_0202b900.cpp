53b5c2e2bf6789adc3903fb85a16bacba7deac99
ZevyaDev <39806174+ZevyaDev@users.noreply.github.com>
2026-09-08 11:34:40 -0400

    Match batch of 1 arm9 main functions

---
+++ src/Combat/Main/func_0202b900.cpp
@@ -0,0 +1,63 @@
+#include <globaldefs.h>
+#include "Combat/Main/BattleList.h"
+
+extern char data_020feffc[];
+
+int GetGlobalField0x10(void);
+int AdvanceStateIfState2(void);
+void SleepCurrentContext(unsigned int);
+extern "C" int _Z12Init0202dc40iPv(int mode, void* src);
+unsigned long long GetCurrentTimestamp(void);
+extern "C" int func_02001aec(void* a, void* b, int n);
+
+// USA: func_0202b900
+extern "C" ARM int func_0202b900(int* obj, unsigned char* key) {
+    char* entries;
+    int i;
+    int result;
+    int count;
+    int j;
+    int stride;
+    struct BattleStruct* battleStruct;
+    unsigned long long ts;
+
+    i = 0;
+    while (GetGlobalField0x10() == 2) {
+        AdvanceStateIfState2();
+        i++;
+        if (i > 0x1388) {
+            return -1;
+        }
+        SleepCurrentContext(1);
+    }
+
+    i = 0;
+    while (GetGlobalField0x10() != 1) {
+        i++;
+        if (i > 0x1388) {
+            return -1;
+        }
+        SleepCurrentContext(1);
+    }
+
+    count = obj[3];
+    result = -1;
+    j = 0;
+    stride = 0xc0;
+    entries = (char*)obj;
+    entries += 0x10;
+
+    for (; j < count; j++) {
+        if (func_02001aec(key, entries + j * stride + 4, 6) == 0) {
+            _Z12Init0202dc40iPv(5, data_020feffc + j * stride);
+            *obj = 6;
+            battleStruct = GetBattleStruct();
+            ts = GetCurrentTimestamp();
+            *(unsigned int*)((char*)battleStruct + 0x3f0) = (unsigned int)ts;
+            result = j;
+            *(unsigned int*)((char*)battleStruct + 0x3f4) = (unsigned int)(ts >> 32);
+            break;
+        }
+    }
+    return result;
+}
